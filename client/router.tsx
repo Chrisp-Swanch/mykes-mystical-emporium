5b85b525f38315e6c52645c8eda25315e4064042
Chris Swan <chrisswan1612@gmail.com>
2023-06-09 14:18:07 +1200

    fix router.tsx

--- client/router.tsx
+++ client/router.tsx
@@ -5,16 +5,17 @@ import {
 } from 'react-router-dom'
 
 import App from './components/App'
-import Form from './components/Form'
+import Home from './components/Home'
 
 import Soulmate from './components/Soulmate'
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<App />}>
-    <Route index element={<Form />} />
-    <Route path='/soulmate' element={<Soulmate />} />
-
-  </Route>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="/soulmate" element={<Soulmate />} />
+    </Route>
+  )
+)
 
 export default router
