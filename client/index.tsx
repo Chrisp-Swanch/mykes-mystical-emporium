b0d6c782235fd58877c562882668d32f940a615a
Chris Swan <chrisswan1612@gmail.com>
2023-06-09 22:59:23 +1200

    styling tweaks

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 
-import App from './components/App'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import './styles/index.scss'
