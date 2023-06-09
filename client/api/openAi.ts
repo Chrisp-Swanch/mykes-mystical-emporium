2d2a95c7d7beee98ede96979db00410218816dfb
Oscar McGoldrick <oscar.mcgoldrick99@gmail.com>
2023-06-09 13:45:06 +1200

    soulmate update

--- client/api/openAi.ts
+++ client/api/openAi.ts
@@ -7,3 +7,10 @@ export function getHoroscope(param: string): Promise<any>{
   .then((res) => {return res})
   .catch(e => console.log(e))
 }
+
+export function getSoulMate(paramOne: string, paramTwo: string): Promise<any>{
+  return request.post('/api/v1/openAI/soulmate')
+  .send({paramOne, paramTwo})
+  .then((res) => {return res})
+  .catch(e => console.log(e))
+}
--- client/router.tsx
+++ client/router.tsx
@@ -6,10 +6,12 @@ import {
 
 import App from './components/App'
 import Form from './components/Form'
+import Soulmate from './components/Soulmate'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<App />}>
     <Route index element={<Form />} />
+    <Route path='/soulmate' element={<Soulmate />} />
   </Route>
 ))
 
